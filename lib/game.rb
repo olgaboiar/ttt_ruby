ab87bb43d13ace36e5dc7e84fd3f838c3a444435
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-11-26 17:01:29 -0500

    started refactoring game class

--- lib/game.rb
+++ lib/game.rb
@@ -8,32 +8,43 @@ require_relative "computer"
 class Game
 
   def start
-    ui = UserInteraction.new
-    language = Language.new(ui)
+    @ui = UserInteraction.new
+    language = Language.new(@ui)
     lang = language.set_lang
-    ui.set_translation(lang)
-    ui.greeting
-    response = ui.get_input
-    ui.bye if !ui.positive(response)
-    if ui.positive(response)
+    @ui.set_translation(lang)
+    @ui.greeting
+    response = @ui.get_input
+    @ui.bye if !@ui.positive(response)
+    if @ui.positive(response)
       markers = Marker.new
-      ui.user_name
-      human_name = ui.get_input
-      @human = Human.new(ui, human_name)
-      ui.difficulty_level
-      difficulty = ui.get_input.to_i
-      @computer = Computer.new(ui, markers, "Computer", difficulty)
-      board = Board.new(ui, markers)
+      set_user_name
+      @human = Human.new(@ui, @human_name)
+      set_difficulty_level
+      @computer = Computer.new(@ui, markers, "Computer", @difficulty)
+      board = Board.new(@ui, markers)
       players = [@human, @computer]
-      tictactoe = TicTacToe.new(players, board, ui)
+      tictactoe = TicTacToe.new(players, board, @ui)
       @hum = @human.set_symbol
       @com = @computer.set_symbol(@hum)
       tictactoe.set_current_player(@hum, @com, @human, @computer)
       @current_player = tictactoe.current_player
-      ui.board
+      @ui.board
       board.print_board
-      ui.first(@current_player.name)
+      @ui.first(@current_player.name)
       tictactoe.play(board)
     end
   end
+
+  def set_user_name
+    @ui.user_name
+    @human_name = @ui.get_input
+    return @human_name
+  end
+
+  def set_difficulty_level
+    @ui.difficulty_level
+    @difficulty = @ui.get_input.to_i
+    return @difficulty
+  end
+
 end
