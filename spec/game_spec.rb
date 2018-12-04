4a40787e3130dc0b11f6c36d0d83ec8da7c42453
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-12-04 16:54:36 -0500

    started refactoring tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,13 +5,26 @@ describe Game do
   before do
     @input = UserInteraction.new
     language = Language.new(@input)
+    markers = Marker.new
     allow_any_instance_of(Language).to receive(:set_lang).and_return('1')
-    @messages = UserMessages.new(language.set_lang)
-    player1 = Human.new(@input, 'V')
-    player2 = Human.new(@input, 'J')
+    player1 = Human.new(@input, markers, 'V')
+    player2 = Human.new(@input, markers, 'J')
     allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
-    @board = Board.new(@input)
+    @board = Board.new(@input, markers)
     tictactoe = TicTacToe.new(player1, player2, @board)
-    @game = Game.new(tictactoe)
+    @game = Game.new
   end
+
+  # describe ' #start' do
+  #   it 'shoul ask the user to play' do
+  #   end
+  # end
+
+  # describe ' #set_mode' do
+  #   it 'shoul ask the user to play' do
+  #     allow_any_instance_of(UserInteraction).to receive(:read_input).and_return('1')
+  #     actual = @game.set_mode
+  #     expect(actual).to eq('1')
+  #   end
+  # end
 end
--- spec/user_interaction_spec.rb
+++ spec/user_interaction_spec.rb
@@ -6,11 +6,137 @@ describe UserInteraction do
     @input = UserInteraction.new
   end
 
-  describe '#get_input' do
+  describe '#read_input' do
     it 'should return input string' do
-      allow_any_instance_of(UserInteraction).to receive(:get_input).and_return('input string')
-      actual = @input.get_input
+      allow_any_instance_of(UserInteraction).to receive(:read_input).and_return('input string')
+      actual = @input.read_input
       expect(actual).to eq('input string')
+    end
+  end
+
+  describe '#choose_lang' do
+    it "should return the string prompting user to choose the language" do
+      expect do
+          @input.choose_lang
+      end.to output("Choose the language: 1 - English, 2 - Ukrainian. \nEnter 1 or 2:\n").to_stdout
+    end
+  end
+
+  describe '#valid_lang(lang)' do
+    it 'should return true when number 1 is entered' do
+      actual = @input.valid_lang('1')
+      expect(actual).to be_truthy
+    end
+
+    it 'should return false when number 22 is entered' do
+      actual = @input.valid_lang('22')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when number 0 is entered' do
+      actual = @input.valid_lang('0')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when letter is entered' do
+      actual = @input.valid_lang('j')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when special character is entered' do
+      actual = @input.valid_lang('~')
+      expect(actual).to be_falsey
+    end
+  end
+
+  describe '#valid_difficulty(lang)' do
+    it 'should return true when number 1 is entered' do
+      actual = @input.valid_difficulty('1')
+      expect(actual).to be_truthy
+    end
+
+    it 'should return false when number 22 is entered' do
+      actual = @input.valid_difficulty('22')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when number 0 is entered' do
+      actual = @input.valid_difficulty('0')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when letter is entered' do
+      actual = @input.valid_difficulty('j')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when special character is entered' do
+      actual = @input.valid_difficulty('~')
+      expect(actual).to be_falsey
+    end
+  end
+
+  describe '#valid_mode(mode)' do
+    it 'should return true when number 1 is entered' do
+      actual = @input.valid_mode('1')
+      expect(actual).to be_truthy
+    end
+
+    it 'should return false when number 22 is entered' do
+      actual = @input.valid_mode('22')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when number 0 is entered' do
+      actual = @input.valid_mode('0')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when number 5 is entered' do
+      actual = @input.valid_mode('5')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when letter is entered' do
+      actual = @input.valid_mode('j')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when special character is entered' do
+      actual = @input.valid_mode('~')
+      expect(actual).to be_falsey
+    end
+  end
+
+  describe '#valid_name(name)' do
+    it 'should return true when dashed name is entered' do
+      actual = @input.valid_name('Anna-Marie')
+      expect(actual).to be_truthy
+    end
+
+    it 'should return false when #name is entered' do
+      actual = @input.valid_name('#Anna')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when : is entered' do
+      actual = @input.valid_name(':ann')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when $ is entered' do
+      actual = @input.valid_name('$anna')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when * is entered' do
+      actual = @input.valid_name('*')
+      expect(actual).to be_falsey
+    end
+
+    it 'should not return true when special character is entered' do
+      actual = @input.valid_name('~')
+      expect(actual).to be_falsey
     end
   end
 
@@ -64,6 +190,57 @@ describe UserInteraction do
 
     it 'should return nil when number is entered' do
       actual = @input.positive(4)
+      expect(actual).to be_falsey
+    end
+  end
+
+  describe 'computer_human(mode)' do
+    it 'should return true when 1 is entered' do
+      actual = @input.computer_human('1')
+      expect(actual).to be_truthy
+    end
+
+    it 'should return nil when 2 is entered' do
+      actual = @input.computer_human('2')
+      expect(actual).to be_falsey
+    end
+
+    it 'should return nil when letter is entered' do
+      actual = @input.computer_human('v')
+      expect(actual).to be_falsey
+    end
+  end
+
+  describe '#human(mode)' do
+    it 'should return true when 2 is entered' do
+      actual = @input.human('2')
+      expect(actual).to be_truthy
+    end
+
+    it 'should return nil when 3 is entered' do
+      actual = @input.human('3')
+      expect(actual).to be_falsey
+    end
+
+    it 'should return nil when letter is entered' do
+      actual = @input.human('v')
+      expect(actual).to be_falsey
+    end
+  end
+
+  describe '#computer(mode)' do
+    it 'should return true when 3 is entered' do
+      actual = @input.computer('3')
+      expect(actual).to be_truthy
+    end
+
+    it 'should return false when 1 is entered' do
+      actual = @input.computer('1')
+      expect(actual).to be_falsey
+    end
+
+    it 'should return nil when etter is entered' do
+      actual = @input.computer('v')
       expect(actual).to be_falsey
     end
   end
@@ -129,6 +306,19 @@ describe UserInteraction do
     it 'should not return nil when special character is entered' do
       actual = @input.check_size('~')
       expect(actual).to be_falsey
+    end
+  end
+
+  describe ' #choose_translation(lang)' do
+    it 'should choose ukrainian yaml file for translations if 2 is passed as an argument' do
+      actual = @input.choose_translation('2')
+      # expect(actual).to be_falsey
+      expect(actual).to eq(YAML.load_file(File.join(__dir__, '../lib/lang/ua.yml')))
+    end
+    it 'should choose ukrainian yaml file for translations if 2 is passed as an argument' do
+      actual = @input.choose_translation('1')
+      # expect(actual).to be_falsey
+      expect(actual).to eq(YAML.load_file(File.join(__dir__, '../lib/lang/en.yml')))
     end
   end
 end
