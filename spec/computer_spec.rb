4fae4a45a0aae81fe3cb7647af3d1baaeed58eb7
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-12-07 14:12:15 -0500

    completed 100% test coverage for Computer class

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -17,6 +17,123 @@ describe Computer do
     end
   end
 
+  describe '#choose_symbol' do
+    it 'should return X' do
+      actual = @computer.choose_symbol
+      expect(actual).to eq('X')
+    end
+  end
+
+  describe '#move(board, symbol)' do
+    it '' do
+      allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
+      board = Board.new(@ui, @markers)
+      board.insert_value(0, 'X')
+      symbol = 'O'
+      allow_any_instance_of(UserInteraction).to receive(:computer_move)
+      @computer.move(board, symbol)
+      actual = board.get_value(4)
+      expect(actual).to eq('O')
+    end
+  end
+
+  describe ' #set_best_move(board, last_move, depth = 0, scores = [], moves = [])' do
+    it 'should return -9' do
+      allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
+      board = Board.new(@ui, @markers)
+      board.insert_value(0, 'X')
+      board.insert_value(4, 'O')
+      board.insert_value(1, 'X')
+      @computer.define_opponent('O')
+      last_move = 'O'
+      actual = @computer.set_best_move(board, last_move)
+      expect(actual).to eq(-9)
+    end
+  end
+
+  describe ' #potential_move(moves, potential_board, spot)' do
+    it 'should return 9' do
+      allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
+      potential_board = Board.new(@ui, @markers)
+      potential_board.insert_value(0, 'X')
+      potential_board.insert_value(5, 'O')
+      potential_board.insert_value(2, 'X')
+      spot = 9
+      moves = [3, 4, 6, 7, 8]
+      @computer.potential_move(moves, potential_board, spot)
+      actual = potential_board.get_value(spot.to_i - 1)
+      expect(actual).to eq(9)
+    end
+  end
+
+  describe ' #return_score(current_move, scores, moves)' do
+    it 'should return 0 as it is the largest digit in scores array' do
+      @computer.define_opponent('X')
+      current_move = 'X'
+      scores = [0, -8, -8, -8]
+      moves = [4, 6, 8, 9]
+      actual = @computer.return_score(current_move, scores, moves)
+      expect(actual).to eq(0)
+    end
+    it 'shoul do something' do
+      @computer.define_opponent('X')
+      current_move = 'O'
+      scores = [0, -8, -8, -8]
+      moves = [4, 6, 8, 9]
+      actual = @computer.return_score(current_move, scores, moves)
+      expect(actual).to eq(-8)
+    end
+  end
+
+  describe '#get_random_move(board)' do
+    it 'should return a random number from 6, 7, 9, 4' do
+      allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
+      board = Board.new(@ui, @markers)
+      board.insert_value(2, 'O')
+      board.insert_value(0, 'O')
+      board.insert_value(4, 'X')
+      board.insert_value(7, 'X')
+      board.insert_value(1, 'O')
+      actual = @computer.get_random_move(board)
+      expect(actual).to eq('7') | eq('6') | eq('9') | eq('4')
+    end
+  end
+
+  describe '#get_move_score(board, last_move, depth)' do
+    it 'should return 0 if the board is tie' do
+      allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
+      board = Board.new(@ui, @markers)
+      board.insert_value(2, 'O')
+      board.insert_value(0, 'X')
+      board.insert_value(4, 'O')
+      board.insert_value(7, 'O')
+      board.insert_value(1, 'X')
+      board.insert_value(3, 'O')
+      board.insert_value(5, 'X')
+      board.insert_value(6, 'X')
+      board.insert_value(8, 'X')
+      last_move = 'X'
+      depth = 1
+      actual = @computer.get_move_score(board, last_move, depth)
+      expect(actual).to eq(0)
+    end
+
+    it 'should return -9 if the board is win depth is 1 and current move is opponents' do
+      allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
+      board = Board.new(@ui, @markers)
+      board.insert_value(2, 'O')
+      board.insert_value(0, 'O')
+      board.insert_value(4, 'X')
+      board.insert_value(7, 'X')
+      board.insert_value(1, 'O')
+      @computer.define_opponent('X')
+      last_move = 'O'
+      depth = 1
+      actual = @computer.get_move_score(board, last_move, depth)
+      expect(actual).to eq(-9)
+    end
+  end
+
   describe '#next_player(last_move)' do
     it 'should return O' do
       marker = @computer.define_opponent('X')
@@ -29,17 +146,4 @@ describe Computer do
       expect(actual).to eq('X')
     end
   end
-
-  # describe ' #move(board, symbol)' do
-  #   it 'place O on the 9th cell' do
-  #     allow(Computer).to receive(:sleep)
-  #     allow_any_instance_of(BoardSize).to receive(:set_size).and_return('3')
-  #     allow_any_instance_of(UserInteraction).to receive(:computer_move)
-  #     @board = Board.new(@input, @markers)
-  #     board_size = BoardSize.new(@input)
-  #     @setup = Setup.new(board_size.set_size)
-  #     @computer.move(@board, 'O')
-  #     expect(@board.get_value(8)).to eq('O')
-  #   end
-  # end
 end
